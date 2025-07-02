f7585664f919bc922c8c236cb345a004989de954
nguyenhoangphuc251 <140061171+nguyenhoangphuc251@users.noreply.github.com>
2025-07-02 19:12:15 +0700

    update

--- Lab5/Ex18/RealTimeClock.js
+++ Lab5/Ex18/RealTimeClock.js
@@ -1,8 +1,9 @@
-function updateClock () {
-    const now = new Date () ;
-    const hours = String ( now.getHours () ).padStart (2 , "0") ;
-    const minutes = String ( now.getMinutes () ).padStart (2 , "0") ;
-    const seconds = String ( now.getSeconds () ).padStart (2 , "0") ;
-    document.getElementById("clock") . innerText = "${ hours }: $ {minutes}: ${ seconds } ";
+function updateClock() {
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2, "0");
+    const minutes = String(now.getMinutes()).padStart(2, "0");
+    const seconds = String(now.getSeconds()).padStart(2, "0");
+    document.getElementById("clock").innerText = `${hours}:${minutes}:${seconds}`;
 }
-setInterval(updateCl);
+setInterval(updateClock, 1000);
+updateClock();
